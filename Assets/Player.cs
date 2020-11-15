280a5d3006df6c79b0968b5ebf11658b5d73e272
Kostas <koszio@gmail.com>
2020-11-15 02:37:48 +0100

    Added a health bar that can be assigned either on a player or on top of the screen

---
+++ Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int health;
+
+
+    public HealthBar healthBar;
+    private void Start()
+    {
+        health = maxHealth;
+        healthBar.SetMaximumHealth(maxHealth);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DamageAmount(20);
+        }
+    }
+
+    private void DamageAmount(int damage)
+    {
+        health -= damage;
+        healthBar.SetHealth(health);
+
+    }
+}
