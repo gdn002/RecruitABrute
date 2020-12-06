5c32660a6f11dadca24a6a9a8745fc7d78724973
Kostas <koszio@gmail.com>
2020-12-06 11:27:24 +0100

    SoundManager Script and set in Unity with Resources Folder to add more audio files

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    public enum Sound
+    {
+        attack,
+
+    }
+
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        Instance = this;
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void PlaySound()
+    {
+        audioSource.PlayOneShot(Resources.Load<AudioClip>(Sound.attack.ToString()));
+
+    }
+}
